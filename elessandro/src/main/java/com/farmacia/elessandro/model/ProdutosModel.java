b70f5dfb4522388a9da0a91af7e608f49a8e20cb
elessandro_abreu <binho_abreu@hotmail.com>
2025-08-28 17:24:18 -0300

    final

--- elessandro/src/main/java/com/farmacia/elessandro/model/ProdutosModel.java
+++ elessandro/src/main/java/com/farmacia/elessandro/model/ProdutosModel.java
@@ -5,29 +5,24 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-
+@Entity
+@Table(name = "TBPRODUTO")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+public class ProdutosModel {
 
-@Entity
-@Table(name = "TBPRODUTO")
-
-
-public class Produtos {
-    @jakarta.persistence.Id
-    private Long id;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cdProduto")
     private Integer cdProduto;
+
     @Column(name = "nmProduto")
     private String nmProduto;
+
     @Column(name = "dsBula")
     private String dsBula;
+
     @Column(name = "vlProduto")
     private Integer vlProduto;
-
-
 }
