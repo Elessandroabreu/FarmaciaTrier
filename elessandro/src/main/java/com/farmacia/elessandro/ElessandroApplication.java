c495f0b229d1686cefcff6f4cdbe553109befc81
elessandro_abreu <binho_abreu@hotmail.com>
2025-08-28 14:20:38 -0300

    iniciando

---
+++ elessandro/src/main/java/com/farmacia/elessandro/ElessandroApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.elessandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElessandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElessandroApplication.class, args);
+	}
+
+}
